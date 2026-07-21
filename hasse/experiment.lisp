65226bb42d7d27679f255d22fbd5c61332755597
LNoelle821 <l.noelle.1@research.gla.ac.uk>
2026-07-21 16:55:10 +0100

    Update experiment.lisp

--- hasse/experiment.lisp
+++ hasse/experiment.lisp
@@ -14,6 +14,7 @@
     (max (if (equal (car p) 0) -1 n)
          (degree (cdr p) (+ 1 n)))))
 
+;; 
 (defun degreerat(num1 num2 den)
   (max( (max(degree num1)
         (degree den))
@@ -27,13 +28,14 @@
   (max(degree num1)
       (degree den1)))
       
-;assume predicate separable exists 
+;;assume predicate separable exists 
 
 (defun point-elliptic-curve(a b x y)
   (equal (* y y)
          (+ (* x x x)
             (* a x)
             b)))  
+      
 (defun power(x q)
   (if (zp q)
       1
@@ -42,5 +44,42 @@
 (defun eval-frobenius (x y q)
   (list (power(x q) power(y q))))
 
+;; definition of field from acl2/books/projects/linear/field.lisp
+
+(encapsulate (((fp *) => *)                   ;field element recognizer
+              ((f+ * *) => *) ((f* * *) => *) ;addition and multiplication
+	      ((f0) => *) ((f1) => *)         ;identities
+	      ((f- *) => *) ((f/ *) => *))    ;inverses 
+  (local (defun fp (x finitefield) ( )))
+  (local (defun f+ (x y) (+ x y)))
+  (local (defun f* (x y) (* x y)))
+  (local (defun f0 () 0))
+  (local (defun f1 () 1))
+  (local (defun f- (x) (- x)))
+  (local (defun f/ (x) (/ x)))
+  ;; Closure:
+  (defthm f+closed (implies (and (fp x) (fp y)) (fp (f+ x y))))
+  (defthm f*closed (implies (and (fp x) (fp y)) (fp (f* x y))))
+  ;; Commutativity
+  (defthmd f+comm (implies (and (fp x) (fp y)) (equal (f+ x y) (f+ y x))))
+  (defthmd f*comm (implies (and (fp x) (fp y)) (equal (f* x y) (f* y x))))
+  ;; Associativity:
+  (defthmd f+assoc (implies (and (fp x) (fp y) (fp z)) (equal (f+ x (f+ y z)) (f+ (f+ x y) z))))
+  (defthmd f*assoc (implies (and (fp x) (fp y) (fp z)) (equal (f* x (f* y z)) (f* (f* x y) z))))
+  ;; Identity:
+  (defthm fpf0 (fp (f0)))
+  (defthm fpf1 (fp (f1)))
+  (defthm f1f0 (not (equal (f1) (f0))))
+  (defthm f0id (implies (fp x) (equal (f+ x (f0)) x)))
+  (defthm f1id (implies (fp x) (equal (f* x (f1)) x)))
+  ;; Inverse:
+  (defthm fpf- (implies (fp x) (fp (f- x))))
+  (defthm fpf/ (implies (and (fp x) (not (equal x (f0)))) (fp (f/ x))))
+  (defthm f+inv (implies (fp x) (equal (f+ x (f- x)) (f0))))
+  (defthm f*inv (implies (and (fp x) (not (equal x (f0)))) (equal (f* x (f/ x)) (f1))))
+  ;; Distributivity:
+  (defthm fdist (implies (and (fp x) (fp y) (fp z)) (equal (f* x (f+ y z)) (f+ (f* x y) (f* x z))))))
+
+      
 
 
