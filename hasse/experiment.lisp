19dd06adc93f782c73cc139f943b069600aefdfe
LNoelle821 <l.noelle.1@research.gla.ac.uk>
2026-07-21 15:46:06 +0100

    Update experiment.lisp

--- hasse/experiment.lisp
+++ hasse/experiment.lisp
@@ -5,18 +5,19 @@
        (* x (eval-poly (cdr coeffs) x)))))
 
 (defun eval-ratfun (num1 num2 den x y)
-  (/ (+(eval-poly num1 x) (* y (eval-poly num2 x)))
+  (/ (+ (eval-poly num1 x) (* y (eval-poly num2 x)))
      (eval-poly den x)))
 
 (defun degree(p n)
   (if (endp p)
       -1
     (max (if (equal (car p) 0) -1 n)
-         (degree-aux (cdr p) (+ 1 n)))))
+         (degree (cdr p) (+ 1 n)))))
 
-(defun degreerat(num den)
-  (max(degree num)
-        (degree den)))
+(defun degreerat(num1 num2 den)
+  (max(degree num1)
+        (degree den)
+      (+ (degree num2) 1) )
 
 
 
