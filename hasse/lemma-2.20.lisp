3285eb180a0fa4789c3cbb58cefccc9439f8ec98
az181 <atticus@gmail.com>
2026-07-23 12:15:33 +0100

    added a lemma 2.20 file but no logic in it

---
+++ hasse/lemma-2.20.lisp
@@ -0,0 +1,15 @@
+( defun separable-endomorphism 
+	())
+; so we take the eliptic curve E and feild Fq and we get phi_q and this is an endomorphism of E and of degree q, and also phi_p is not sepribable 
+; what we need for this 
+; eliptic curves 
+; - additon 
+; finit feilds of card q 
+; phi_q 
+; def of endomorphisms
+; separabilty of endomorphisms 
+
+( defthm lemma-2.20
+	 (implise ))
+
+
