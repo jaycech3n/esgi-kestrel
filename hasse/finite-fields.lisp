26a524e92aae40f51607c84c8c81a9b4b6a6846f
Gihan Marasingha <gihan.marasingha@gmail.com>
2026-07-27 14:26:26 +0100

    add comments add resutls concerning the reduction of a polynomial

--- hasse/finite-fields.lisp
+++ hasse/finite-fields.lisp
@@ -1,31 +1,63 @@
 ; Executable prime fields and simple polynomial quotient extensions.
 
+; We start with primitive functions for working over prime fields and
+; with polynomials over prime fields. These functions and theorems are denoted
+; with a pf prefix. 
+
+
+; This book defines functions for working with polynomials over prime 'fields'.
+; We don't impose any structure on prime 
+; The prefix pf is used to denote functions and theroems that relate to prime
+; fields.
+
+; In this book, we use
+; A polynomial is merely a list of coefficients.
+
 (in-package "ACL2")
 
 (include-book "kestrel/arithmetic-light/mod" :dir :system)
 
+;; ## Primitive functions for prime fields and polynomials over prime fields.
+;;
+;; In this section, we'll fix the field to be F_p for a prime p.
+;; The 'field' here is not yet the data structure we will define later.
+
+
+; Normalizes an element x modulo p, usually reduction of x mod p.
+; Uses default 0 for x if x is not an integer.
+; Uses default 2 for p if p is not an integer or p <= 1.
 (defun pf-normalize (x p)
   (mod (ifix x) (if (and (integerp p) (< 1 p)) p 2)))
 
+
+; Is x an element of the prime field?
+; Returns T if x if x and p are natural numbers with 1 < p and 0 <= x < p.
 (defun pf-element-p (x p)
   (and (natp x) (natp p) (< 1 p) (< x p)))
 
+; If p is a natural number and 1 < p then the the normalization of x mod p
+; is an element of the prime field.
 (defthm pf-element-p-of-pf-normalize
   (implies (and (natp p) (< 1 p))
-           (pf-element-p (pf-normalize x p) p))
-  :hints (("Goal"
-           :in-theory (enable pf-element-p pf-normalize))))
+           (pf-element-p (pf-normalize x p) p)))
 
+; A brute force function that checks if p has no integer factors x
+; in the range 1 < x <= d
+; Returns T if d is not a natural number or if d < 2.
 (defun no-divisor-at-or-below-p (p d)
   (if (or (not (natp d)) (< d 2))
       t
     (and (not (equal (mod p d) 0))
          (no-divisor-at-or-below-p p (1- d)))))
 
+; Check if p is prime by testing if p is a natural number greater than 1 and
+; testing if it has no integer factors x in the range  1 < x <= floor(p/2).
 (defun prime-number-p (p)
   (and (natp p) (< 1 p)
        (no-divisor-at-or-below-p p (floor p 2))))
 
+; Removes trailing zeros from polynomials. Here, a polynomial is treated simply
+; as a list (possibly empty) of coefficients.
 (defun pf-poly-trim (poly)
   (if (endp poly)
       nil
@@ -34,17 +66,22 @@
           nil
         (cons (car poly) rest)))))
 
+; Do the cofficients of the polynomial poly belong to the prime field?
 (defun pf-poly-coefficients-p (poly p)
   (if (endp poly)
       (equal poly nil)
     (and (pf-element-p (car poly) p)
          (pf-poly-coefficients-p (cdr poly) p))))
 
+; Is poly a pf-polynomial? For this to be the case,
+; poly must be a list, with coefficients in the field and it must already
+; be trimmed of trailing zeros.
 (defun pf-polynomial-p (poly p)
   (and (true-listp poly)
        (pf-poly-coefficients-p poly p)
        (equal (pf-poly-trim poly) poly)))
 
+; Trims the trailing zeros and normalises the coefficients of the polynomial poly.
 (defun pf-poly-normalize (poly p)
   (if (endp poly)
       nil
@@ -52,10 +89,12 @@
      (cons (pf-normalize (car poly) p)
            (pf-poly-normalize (cdr poly) p)))))
 
+; The degree of the polynomial poly. This is 0 for the 0 polynomial.
 (defun pf-poly-degree (poly)
   (let ((poly (pf-poly-trim poly)))
     (if (endp poly) 0 (1- (len poly)))))
 
+; Addition of polynomials.
 (defun pf-poly-add (x y p)
   (declare (xargs :measure (+ (acl2-count x) (acl2-count y))))
   (if (and (endp x) (endp y))
@@ -66,38 +105,49 @@
            (pf-poly-add (if (consp x) (cdr x) nil)
                         (if (consp y) (cdr y) nil) p)))))
 
+; If the coefficients of the polynomial poly belong to the prime field then so
+; do the coefficients of the trimmed polynomial.
 (defthm pf-poly-coefficients-p-of-pf-poly-trim
   (implies (pf-poly-coefficients-p x p)
            (pf-poly-coefficients-p (pf-poly-trim x) p))
   :hints (("Goal" :induct (pf-poly-trim x))))
 
+; The pf-poly-trim function is idempotent.
 (defthm pf-poly-trim-idempotent
   (equal (pf-poly-trim (pf-poly-trim x))
          (pf-poly-trim x))
   :hints (("Goal" :induct (pf-poly-trim x))))
 
+; The sum (over the prime field) of two polynomials have coefficients in the
+; prime field (assuming p is a natural number greater than 1)
 (defthm pf-poly-coefficients-p-of-pf-poly-add
   (implies (and (natp p) (< 1 p))
            (pf-poly-coefficients-p (pf-poly-add x y p) p))
   :hints (("Goal" :induct (pf-poly-add x y p))))
 
+; The sum (over the prime field) of two polynomials is a pf-polynomial.
 (defthm pf-polynomial-p-of-pf-poly-add
   (implies (and (natp p) (< 1 p))
            (pf-polynomial-p (pf-poly-add x y p) p))
   :hints (("Goal"
            :in-theory (enable pf-polynomial-p))))
 
+; The length of a trimmed polynomial x is no more than the length of x.
 (defthm len-of-pf-poly-trim-upper-bound
   (<= (len (pf-poly-trim x)) (len x))
   :rule-classes :linear
   :hints (("Goal" :induct (pf-poly-trim x))))
 
+; The length of the sum of two polynomials is at most the maximum of the
+; lengths of the polynomials.
 (defthm len-of-pf-poly-add-upper-bound
   (<= (len (pf-poly-add x y p))
       (max (len x) (len y)))
   :rule-classes :linear
   :hints (("Goal" :induct (pf-poly-add x y p))))
 
+; If the x and y are polynomials of length less than n, then their sum has
+; length less than n.
 (defthm len-of-pf-poly-add-when-inputs-bounded
   (implies (and (< (len x) n)
                 (< (len y) n))
@@ -106,6 +156,7 @@
            :use len-of-pf-poly-add-upper-bound
            :cases ((<= (len x) (len y))))))
 
+; The (trimmed and normalized) negative of a polynomial x.
 (defun pf-poly-neg (x p)
   (if (endp x)
       nil
@@ -113,9 +164,11 @@
      (cons (pf-normalize (- (car x)) p)
            (pf-poly-neg (cdr x) p)))))
 
+; Polynomial subtraction
 (defun pf-poly-sub (x y p)
   (pf-poly-add x (pf-poly-neg y p) p))
 
+; Scaling (and trimming and normalizing) a polynomial x by a constant c.
 (defun pf-poly-scale (c x p)
   (if (endp x)
       nil
@@ -123,15 +176,32 @@
      (cons (pf-normalize (* c (car x)) p)
            (pf-poly-scale c (cdr x) p)))))
 
+; Multiplication of two polynomials
 (defun pf-poly-mul (x y p)
   (if (or (endp x) (endp y))
       nil
     (pf-poly-add (pf-poly-scale (car x) y p)
                  (cons 0 (pf-poly-mul (cdr x) y p)) p)))
 
+
+; The leading coefficient of a polynomial (or zero). Not sure if I need this.
+(defun pf-poly-leading-coefficient (modulus p)
+  (if (endp modulus) 0
+  (last
+   (pf-poly-trim (pf-normalize modulus p))
+   )))
+
+
+; A list of n zeros.
 (defun zeros (n)
   (if (zp n) nil (cons 0 (zeros (1- n)))))
 
+; Returns x - g * modulus, where g is chosen so that
+; (if modulus is monic and if x and modulus are normalized),
+; then the result has lower degree than x (or is zero).
+; This is the first step in reduction modulo modulus.
+;
+; NOTE: this function returns the 'wrong' result if modulus isn't monic.
 (defun pf-poly-reduce-once (x modulus p)
   (let* ((x (pf-poly-trim x))
          (modulus (pf-poly-trim modulus))
@@ -142,6 +212,25 @@
                          (pf-poly-scale lead modulus p))
                  p)))
 
+; If x has degree at least one and if modulus
+; is a monic polynomial of degree at least one, then
+; pf-poly-reduce-once x modulus p
+; has degree less than the degree of x
+
+(defthm pf-poly-reduce-once-degree
+  (let* ((x (pf-poly-trim (pf-normalize x p)))
+         (modulus (pf-poly-trim (pf-normalize modulus p))))
+  (implies
+   (and
+    (< 1 (pf-poly-degree x))
+    (< 1 (pf-poly-degree modulus))
+    (equal (pf-poly-leading-coefficient modulus p) 1))
+   (equal (pf-poly-degree (pf-poly-reduce-once x modulus p))
+          (1- (pf-poly-degree x))))))
+
+
+; An auxiliary function used in computing the reduction of x modulo modulus.
+; (assuming modulus is monic)
 (defun pf-poly-mod-aux (x modulus p fuel)
   (declare (xargs :measure (nfix fuel)))
   (let ((x (pf-poly-trim x))
@@ -150,11 +239,35 @@
         x
       (pf-poly-mod-aux (pf-poly-reduce-once x modulus p)
                        modulus p (1- fuel)))))
+  
 
+; The reduction of x modulo the monic polynonial modulus.
 (defun pf-poly-mod (x modulus p)
   (pf-poly-mod-aux (pf-poly-normalize x p)
                    (pf-poly-normalize modulus p)
                    p (len x)))
+
+; pf-poly-mod is idempotent (assuming 
+(defthm pf-poly-mod-idempotent
+    (let* ((x (pf-poly-trim (pf-normalize x p)))
+           (modulus (pf-poly-trim (pf-normalize modulus p))))
+      (implies
+       (and
+        (< 1 (pf-poly-degree modulus))
+        (equal (pf-poly-leading-coefficient modulus p) 1)
+        )
+       
+       (equal
+        (pf-poly-mod (pf-poly-mod x modulus p) modulus p)
+        (pf-poly-mod x modulus p)) )))
+
+
+
+;(defthm len-of-pf-poly-mod-upper-bound
+;  (implies (and (consp modulus) (consp x))
+;           (< (len (pf-poly-mod x modulus p))
+;              (len modulus))))
+
 
 (defun pf-poly-eval-horner (poly x p)
   (if (endp poly)
@@ -246,7 +359,15 @@
   (if (equal (ff-kind field) :extension)
       (pf-poly-mod (pf-poly-mul x y (ff-characteristic field))
                    (ff-modulus field) (ff-characteristic field))
-    (pf-normalize (* (ifix x) (ifix y)) (ff-characteristic field))))
+      (pf-normalize (* (ifix x) (ifix y)) (ff-characteristic field))))
+
+#|
+(defthm ff-mul-closed
+  (implies (and (ff-field-p field)
+                (ff-element-p x field)
+                (ff-element-p y field))
+(ff-element-p (ff-mul x y field) field)))
+|#
 
 (defun ff-pow (x n field)
   (if (zp n)
